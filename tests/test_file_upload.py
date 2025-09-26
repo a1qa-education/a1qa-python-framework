461f4df42bf2f7f5e2c76f4f2077902034ab4d38
Stephanie Penaloza <s.penaloza@a1qa.com>
2025-09-25 20:16:15 -0600

    add file upload test

---
+++ tests/test_file_upload.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+
+from configs.settings import TEST_APP_URL
+from framework.ui.browser.browser import Browser
+from pages.file_upload_page import FileUploadPage
+from pages.main_page import MainPage
+
+
+@pytest.mark.file_upload
+class TestFileUpload:
+
+    # Implement test:
+    pass
